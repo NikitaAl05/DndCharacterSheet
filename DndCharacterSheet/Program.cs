fe2a0f58bed7c39ac2d03086568d76e39268c934
NikitaAl05 <alexandrov.n05@icloud.com>
2026-08-01 22:48:00 +0400

    feat: добавил не большой ui

--- DndCharacterSheet/Program.cs
+++ DndCharacterSheet/Program.cs
@@ -7,16 +7,8 @@ class Program
         Character hero = new Character("Никита", CharacterRace.Human, CharacterClass.Fighter);
 
         hero.SetStats(20,10,16,5,5,8);
-
-        Console.WriteLine($"Current HP: {hero.CurrentHealth}");
-        hero.TakeDamage(10);
-        Console.WriteLine($"Current HP: {hero.CurrentHealth}");
         
-        CharacterStorage.Save(hero, "save.json");
+        ConsoleUi.DisplaySheet(hero);
         
-        Character loadHero = CharacterStorage.Load("save.json");
-        Console.WriteLine(loadHero.CurrentHealth);
-        Console.WriteLine($"Current HP: {loadHero.CurrentHealth}");
-
     }
 }
